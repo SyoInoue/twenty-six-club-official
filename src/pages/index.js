07997848082f290aadf43a47d326c2db14d13618
SyoInoue <funnyfacee7777@gmail.com>
2024-02-25 08:54:35 +0900

    頭文字を大文字に変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Header from '../components/header'
-import Main from '../components/main'
-import Footer from '../components/footer'
+import Header from '../components/Header'
+import Main from '../components/Main'
+import Footer from '../components/Footer'
 import Layout from '../components/layout'
 
 export default class IndexPage extends React.Component {
