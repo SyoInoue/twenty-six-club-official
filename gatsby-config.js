8324fe1b2b8d24e097d7861f5dff9fe37ffc48f4
SyoInoue <funnyfacee7777@gmail.com>
2024-03-10 22:56:42 +0900

    SEO強化

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,26 @@
 module.exports = {
   siteMetadata: {
-    title: 'TWENTY SIX CLUB OFFICIAL WEBSITE',
-    author: 'Syo Inoue',
-    description: 'TWENTY SIX CLUB OFFICIAL WEBSITE',
+    title: `TWENTY SIX CLUB OFFICIAL WEBSITE`,
+    author: `Syo Inoue`,
+    description: `TWENTY SIX CLUB OFFICIAL WEBSITE`,
+    siteUrl: `https://www.twentysixclub.jp`,
   },
   plugins: [
-    'gatsby-plugin-typescript',
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-typescript`,
     {
       resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-robots-txt',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#000000',
-        theme_color: '#000000',
-        display: 'minimal-ui',
-        icon: 'src/images/tsc-logo-favicon.png',
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
+        display: `minimal-ui`,
+        icon: `src/images/tsc-logo-favicon.png`,
       },
     },
-    'gatsby-plugin-sass',
+    `gatsby-plugin-sass`,
   ],
 }
